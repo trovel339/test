3f6de1a2798e4a68b281810ce14648396360a313
trovel399 <whylinux.akm@gmail.com>
2023-05-11 22:37:29 +0630

    first commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+
+
+const myName = "Aung Kyae Mhon";
+
+console.log(myName);
+
+// အသေ
+
+// function intoft(){
+//     let inches = 60;
+//     let unitconvert = 1/12;
+//     return inches * unitconvert;
+// }
+
+// console.log(intoft());
+// console.log(intoft());
+
+// အရှင်
+
+// function inToft(inches){
+//     return inches/12 +"ft"
+// }
+
+// console.log(inToft(120));
+
+// array
+
+// let fruits = ["apple","orange","mango","banana"];
+
+
+
+// fruits[0] = "aa apple";
+// console.log(fruits);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+
+
+
+let mySelf = [
+    "Aung Kyae Mhon",
+    19,
+    null,
+    "developer",
+    ["html","css","js"],
+    "Bago",
+    100000,
+    false
+]
+
+console.log(mySelf);
+console.log(`My name is ${mySelf[0]} and iam ${mySelf[1]} years old.
+I live in ${mySelf[5]} and my job is ${mySelf[3]} and i understand ${mySelf[4][2]}`
+);
+
+
+
+
---
+++ js/backup.js
@@ -0,0 +1,67 @@
+        // function run(){
+
+        //     let x = 5;
+        //     let myName = "Aungkyaemhon";
+        //     let myAge = 19;
+        //     let myJob = "developer";
+
+        //     console.log("This is",x)
+        //     console.log(`my name is ${myName} and my age is ${myAge} years old`)
+
+        //     console.log("I am run")
+        //     return "This is a run"
+        // };
+
+        // run()
+        // console.log(run())
+
+
+        
+        // function showMark(){
+        //     let mark =100;
+        //     return "hello"
+            
+        // }
+
+        // console.log(showMark())
+
+
+        // let mark = 40;
+        // console.log(mark >= 40);
+
+        // let a = 4;
+        // let b = "4";
+        // console.log(a == b);
+        // console.log(a === b);
+
+
+        // let x = 5;
+        // let myName = "Aungkyaemhon";
+        // let myAge = 19;
+        // let myJob = "developer";
+
+        // console.log(x, typeof x);
+        // console.log(myName, typeof myName);
+
+        // console.log("my name is "+myName+" and my age is "+myAge+" years old")
+        // console.log(`my name is ${myName} and my age is ${myAge} years old`)
+
+        // const y = 10;
+        // console.log(y);
+        // y = 5;
+        // console.log(y);
+        
+        // let x = 5;
+        // console.log(x);
+        // x = 10;
+        // console.log(x);
+
+        // const y = 10;
+
+        // console.log(x, myName, myAge);
+
+        // console.log(x);
+        // console.log(myName);
+        // console.log(myAge);
+        // console.log(myJob);
+        // console.log(y);
